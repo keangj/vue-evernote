981396b8cf8b557701c80a71a58ae78bd96c6b97
jay <keangj@outlook.com>
2018-06-27 14:31:21 +0800

    修复了ie浏览器不兼容的问题

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import 'babel-polyfill'
 // import ElementUI from 'element-ui'
-import { Message, MessageBox, Dropdown, DropdownMenu, DropdownItem } from 'element-ui'
+import { Message, MessageBox, Dropdown, DropdownMenu, DropdownItem, Notification } from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css';
 import App from './App'
 import router from './router'
